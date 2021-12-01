bc96f3e82b282d5873e3504ddbba29267feeabae
Adam <adampblair@protonmail.com>
2021-12-01 11:05:12 -0700

    Magisk

--- hooks/aosp_build_pre.sh
+++ hooks/aosp_build_pre.sh
@@ -8,8 +8,8 @@ patch -p1 --no-backup-if-mismatch < "${AOSP_BUILD_DIR}/platform/prebuilts/microg
 cd "${AOSP_BUILD_DIR}/frameworks/base"
 patch -p1 --no-backup-if-mismatch < "${AOSP_BUILD_DIR}/platform/prebuilts/microg/00002-microg-sigspoof.patch"
 
-find ${AOSP_BUILD_DIR} -name mkbootfs.c
-tree -d ${AOSP_BUILD_DIR}
+find "${AOSP_BUILD_DIR}" -name mkbootfs.c
+tree -d "${AOSP_BUILD_DIR}"
 
 cd "${AOSP_BUILD_DIR}/frameworks/base"
 patch -p1 --no-backup-if-mismatch < "${AOSP_BUILD_DIR}/platform/prebuilts/magisk/00003-magisk_mkbootfs.patch"
